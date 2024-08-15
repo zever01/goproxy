cfb7d686316d4c314609aadfa81a52d4080f57d3
zever01 <32920307+zever01@users.noreply.github.com>
2024-08-15 19:57:40 +0900

    Update image.go

--- ext/image/image.go
+++ ext/image/image.go
@@ -2,8 +2,8 @@ package goproxy_image
 
 import (
 	"bytes"
-	. "github.com/elazarl/goproxy"
-	"github.com/elazarl/goproxy/regretable"
+	. "github.com/zever01/goproxy"
+	"github.com/zever01/goproxy/regretable"
 	"image"
 	_ "image/gif"
 	"image/jpeg"
