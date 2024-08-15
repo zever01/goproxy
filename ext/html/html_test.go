34ce1625c50dfb09099e5dc29797060debd6785d
zever01 <32920307+zever01@users.noreply.github.com>
2024-08-15 19:57:55 +0900

    Update html_test.go

--- ext/html/html_test.go
+++ ext/html/html_test.go
@@ -1,8 +1,8 @@
 package goproxy_html_test
 
 import (
-	"github.com/elazarl/goproxy"
-	"github.com/elazarl/goproxy/ext/html"
+	"github.com/zever01/goproxy"
+	"github.com/zever01/goproxy/ext/html"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
